2ddb5993431d847d2c895e373d3035c0f831925e
mxxt1 <matt@dustcloudlabs.com>
2019-09-05 14:02:33 -0400

    add axios get request, added data response and reviewed data response

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,12 +3,51 @@
            https://api.github.com/users/<your name>
 */
 
+axios.get('https://api.github.com/users/mxxt1')
+.then(response =>{
+  console.log(`API Response: `,response);
+});
+
+
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
 
    Skip to Step 3.
 */
+//*******************GITHUB API RESPONSE****************************/
+// avatar_url: "https://avatars3.githubusercontent.com/u/51805051?v=4"
+// bio: "Product Marketer and Web Developer with a focus on early-stage startups and small- and mid-market businesses."
+// blog: "www.altavista.com"
+// company: "DustCloud Labs"
+// created_at: "2019-06-13T23:28:41Z"
+// email: null
+// events_url: "https://api.github.com/users/mxxt1/events{/privacy}"
+// followers: 25
+// followers_url: "https://api.github.com/users/mxxt1/followers"
+// following: 31
+// following_url: "https://api.github.com/users/mxxt1/following{/other_user}"
+// gists_url: "https://api.github.com/users/mxxt1/gists{/gist_id}"
+// gravatar_id: ""
+// hireable: null
+// html_url: "https://github.com/mxxt1"
+// id: 51805051
+// location: "Baltimore"
+// login: "mxxt1"
+// name: "Matt G"
+// node_id: "MDQ6VXNlcjUxODA1MDUx"
+// organizations_url: "https://api.github.com/users/mxxt1/orgs"
+// public_gists: 0
+// public_repos: 22
+// received_events_url: "https://api.github.com/users/mxxt1/received_events"
+// repos_url: "https://api.github.com/users/mxxt1/repos"
+// site_admin: false
+// starred_url: "https://api.github.com/users/mxxt1/starred{/owner}{/repo}"
+// subscriptions_url: "https://api.github.com/users/mxxt1/subscriptions"
+// type: "User"
+// updated_at: "2019-09-05T17:49:05Z"
+// url: "https://api.github.com/users/mxxt1"
 
 /* Step 4: Pass the data received from Github into your function, 
            create a new component and add it to the DOM as a child of .cards
